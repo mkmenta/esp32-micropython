2202fbe58f7af35a28e480629184082988ea6f9a
mkmenta <mkmenta95@gmail.com>
2025-07-13 12:44:38 +0200

    Add IFCONFIG

--- src/boot.py
+++ src/boot.py
@@ -3,7 +3,7 @@ from wifi_config import WIFI_NETWORKS
 from connect_wifi import connect_wifi
 
 # Connect to WiFi using the provided networks
-connect_wifi(WIFI_NETWORKS)
+IFCONFIG = connect_wifi(WIFI_NETWORKS)
 
 # Start the WebREPL server
 webrepl.start()
