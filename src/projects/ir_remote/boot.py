bfeeb9c712df6825affc9fb91d58b0c445e06519
mkmenta <mkmenta95@gmail.com>
2025-07-13 15:56:56 +0200

    Cleanup

--- src/projects/ir_remote/boot.py
+++ src/projects/ir_remote/boot.py
@@ -11,43 +11,39 @@ IFCONFIG = connect_wifi(WIFI_NETWORKS)
 sws = SimpleWebServer(IFCONFIG[0])
 
 integrated_led = machine.Pin(2, machine.Pin.OUT)
+# Initialize the LED state
+integrated_led.value(0)
+IS_ACTIVE = False
 
-def toggle_light(query, request):
-    """Example handler to toggle a light."""
+def toggle_onoff(query, request):
     if not query:
         query = extract_json_from_request(request)
     if 'state' in query and query['state'] == 'on':
-        integrated_led.value(1)  # Turn on the integrated LED
-        return json.dumps({"is_active":True})
+        integrated_led.value(1)
+        global IS_ACTIVE
+        IS_ACTIVE = True
+        return json.dumps({"is_active": IS_ACTIVE})
     if 'state' in query and query['state'] == 'off':
         integrated_led.off()
-        return json.dumps({"is_active":False})
+        global IS_ACTIVE
+        IS_ACTIVE = False
+        return json.dumps({"is_active": IS_ACTIVE})
     raise Exception
 
-def turn_on_light(query, request):
-    """Example handler to turn on a light."""
-    return toggle_light({'state': 'on'}, request)
+def turn_on(query, request):
+    return toggle_onoff({'state': 'on'}, request)
 
-def turn_off_light(query, request):
-    """Example handler to turn off a light."""
-    return toggle_light({'state': 'off'}, request)
+def turn_off(query, request):
+    return toggle_onoff({'state': 'off'}, request)
 
-def full_request_handler(query, request):
-    """Example handler that returns the full request as html."""
-    return f"<h1>Full Request</h1><pre>{request}</pre>"
-
-def light_status(query, request):
+def active_status(query, request):
     """Example handler to get the light status."""
-    return json.dumps({"is_active": integrated_led.value() == 1})
+    return json.dumps({"is_active": IS_ACTIVE})
 
-sws.add_handler('POST', '/toggle_light', toggle_light, content_type='application/json')
-sws.add_handler('GET', '/toggle_light', light_status, content_type='application/json')
-sws.add_handler('GET', '/turn_on_light', turn_on_light, content_type='application/json')
-sws.add_handler('GET', '/turn_off_light', turn_off_light, content_type='application/json')
-sws.add_handler('GET', '/full_request', full_request_handler, content_type='text/html')
-sws.add_handler('POST', '/full_request', full_request_handler, content_type='text/html')
-sws.add_handler('PUT', '/full_request', full_request_handler, content_type='text/html')
-sws.add_handler('DELETE', '/full_request', full_request_handler, content_type='text/html')
+sws.add_handler('POST', '/toggle_onoff', toggle_onoff, content_type='application/json')
+sws.add_handler('GET', '/toggle_onoff', active_status, content_type='application/json')
+sws.add_handler('GET', '/turn_on', turn_on, content_type='application/json')
+sws.add_handler('GET', '/turn_off', turn_off, content_type='application/json')
 
 # Start the web server
 # sws.start()
